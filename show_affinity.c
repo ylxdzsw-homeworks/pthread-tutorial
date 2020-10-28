cc63b36fae9a906bd1d4d70135c6e29de04a6ca2
zhongyuchen <izhongyuchen@gmail.com>
2020-10-29 01:10:45 +0800

    update

--- show_affinity.c
+++ show_affinity.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <sys/sysinfo.h>
 
 int main(int argc, char *argv[]) {
   int rc;
@@ -14,13 +15,15 @@ int main(int argc, char *argv[]) {
   rc = pthread_getaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
   assert(rc == 0);
   printf("Before setting: pthread_getaffinity_np() contained:\n");
+  printf("CPU ");
   for (int j = 0; j < CPU_SETSIZE; j++)
     if (CPU_ISSET(j, &cpuset))
-      printf("    CPU %d\n", j);
+      printf("%d ", j);
+  printf("\n");
 
   printf("Set affinity mask to include CPUs (1, 3, 5, ...  2n+1)\n");
   CPU_ZERO(&cpuset);
-  for (int j = 0; j < 8; j++)
+  for (int j = 0; j < get_nprocs(); j++)
     if (j % 2 == 1)
       CPU_SET(j, &cpuset);
   rc = pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
@@ -29,9 +32,11 @@ int main(int argc, char *argv[]) {
   rc = pthread_getaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
   assert(rc == 0);
   printf("After setting: pthread_getaffinity_np() contained:\n");
+  printf("CPU ");
   for (int j = 0; j < CPU_SETSIZE; j++)
     if (CPU_ISSET(j, &cpuset))
-      printf("    CPU %d\n", j);
+      printf("%d ", j);
+  printf("\n");
 
   return 0;
 }
