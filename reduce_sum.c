33cc0988fdd76db08ef64bcc8ad353590f86ff25
zhongyuchen <izhongyuchen@gmail.com>
2020-10-29 03:07:13 +0800

    update

--- reduce_sum.c
+++ reduce_sum.c
@@ -1,21 +1,18 @@
 #define _GNU_SOURCE
 #include <assert.h>
+#include <math.h>
 #include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/sysinfo.h>
 #include <time.h>
-#include <math.h>
 
 typedef struct _arg_t {
-  float *arr;
+  float *dst;
+  float *src;
   size_t len;
 } arg_t;
-
-typedef struct _ret_t {
-  float sum;
-} ret_t;
 
 void init_array(float *p, size_t len) {
   for (int i = 0; i < len; ++i) {
@@ -35,18 +32,17 @@ float *gen_array(size_t len) {
 void *reduce_sum(void *args) {
   arg_t *vec = (arg_t *)args;
 
+  float *dst = vec->dst;
+  float *src = vec->src;
   size_t len = vec->len;
-  double sum = 0.0f;
   for (int i = 0; i < len; ++i) {
-    sum += fabs(vec->arr[i]);
+    dst[i] += src[i];
   }
 
-  ret_t *ret = (ret_t *)malloc(sizeof(ret_t));
-  ret->sum = sum;
-  return ret;
+  return NULL;
 }
 
-void run(float *arr, int len, arg_t *args, int k, int flag) {
+void run(arg_t *args, int k, int flag) {
   pthread_t ph[k];
   int rc;
   cpu_set_t cpu_set[k];
@@ -74,28 +70,16 @@ void run(float *arr, int len, arg_t *args, int k, int flag) {
     assert(rc == 0);
   }
 
-  ret_t *rets[k];
   for (int i = 0; i < k; ++i) {
-    rc = pthread_join(ph[i], (void **)&rets[i]);
+    rc = pthread_join(ph[i], NULL);
     assert(rc == 0);
   }
-
-  double sum = 0.0;
-  for (int i = 0; i < k; ++i) {
-    sum += rets[i]->sum;
-  }
-
-  printf("final sum=%.6f\n", sum);
 
   clock_gettime(CLOCK_MONOTONIC_RAW, &end);
 
   uint64_t delta_us = (end.tv_sec - start.tv_sec) * 1.0e6 +
                       (end.tv_nsec - start.tv_nsec) * 1.0e-3;
-  printf("[Merge sort]The elapsed time is %.2f ms.\n", delta_us / 1000.0);
-
-  for (int i = 0; i < k; ++i) {
-    free(rets[i]);
-  }
+  printf("The elapsed time is %.2f ms.\n", delta_us / 1000.0);
 }
 
 int main(int argc, char *argv[]) {
@@ -110,8 +94,10 @@ int main(int argc, char *argv[]) {
 
   srand(time(NULL));
 
-  float *arr = gen_array(num);
-  init_array(arr, num);
+  float *dst = gen_array(num);
+  float *src = gen_array(num);
+  init_array(dst, num);
+  init_array(src, num);
 
   int chunk_size = num / k;
   int r = num % k;
@@ -120,15 +106,15 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < k; ++i) {
     lo = hi;
     hi += chunk_size;
-    args[i] = (arg_t){arr + lo, hi - lo};
+    args[i] = (arg_t){dst + lo, src + lo, hi - lo};
   }
   args[k - 1].len += r;
 
   // without setting affinity
-  run(arr, num, args, k, 0);
+  run(args, k, 0);
 
   // with setting affinity
-  run(arr, num, args, k, 1);
+  run(args, k, 1);
 
   return 0;
 }
