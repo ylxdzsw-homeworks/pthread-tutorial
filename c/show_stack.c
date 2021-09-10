f0a946311e584fb8bfe9336dcda0aa84ef6cc41a
iamhyc <sudofree@163.com>
2021-09-10 15:54:31 +0800

    update show_stack

--- c/show_stack.c
+++ c/show_stack.c
@@ -1,60 +1,112 @@
 #define _GNU_SOURCE /* To get pthread_getattr_np() declaration */
-#include <assert.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+// static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-void show_stack(pthread_attr_t *attr, pthread_t thread, char *prefix) {
+void print_error_msg(const char *idx_str, const char *msg, int ret)
+{
+  switch (ret)
+  {  
+  case EAGAIN:  // `pthread_create`: system-imposed limit, or insufficient resources.
+    fprintf(stderr, "[%s] %s: EAGAIN\n", idx_str, msg);
+    break;
+  case EPERM:   // `pthread_create`: no permission to set params in `attr`.
+    fprintf(stderr, "[%s] %s: EPERM\n", idx_str, msg);
+    break;
+  case EDEADLK: // `pthread_join`: deadlock detected.
+    fprintf(stderr, "[%s] %s: EDEADLK\n", idx_str, msg);
+    break;
+  case EINVAL:  // `pthread_create` or `pthread_join` or `pthread_attr_getstack`: invalid value.
+    fprintf(stderr, "[%s] %s: EINVAL\n", idx_str, msg);
+    break;
+  case ESRCH:   // `pthread_join`: no thread id found.
+    fprintf(stderr, "[%s] %s: ESRCH\n", idx_str, msg);
+    break;
+  case ENOMEM:  // `pthread_getattr_np`: insufficient memory.
+    fprintf(stderr, "[%s] %s: ENOMEM\n", idx_str, msg);
+    break;
+  default:
+    fprintf(stderr, "[%s] %s: %d\n", idx_str, msg, ret);
+    break;
+  }
+}
+
+void show_stack(pthread_attr_t *attr, pthread_t thread, char *idx_str) {
+  int ret = 0;
   size_t stack_size, guard_size;
   void *stack_addr;
-  int rc;
 
-  rc = pthread_attr_getguardsize(attr, &guard_size);
-  assert(rc == 0);
+  // get the guard size (unit: byte)
+  if ( (ret=pthread_attr_getguardsize(attr, &guard_size)) != 0 )
+  {
+    print_error_msg(idx_str, "pthread_attr_getguardsize failed", ret);
+  }
+  // get the stack attribute values size
+  if ( (ret=pthread_attr_getstack(attr, &stack_addr, &stack_size)) !=0 )
+  {
+    print_error_msg(idx_str, "pthread_attr_getstack failed", ret);
+  }
 
-  rc = pthread_attr_getstack(attr, &stack_addr, &stack_size);
-  assert(rc == 0);
-
-  printf("Thread %s (id=%lu) stack:\n", prefix, thread);
-  printf("\tstart address\t= %p\n", stack_addr);
-  printf("\tend address\t= %p\n", stack_addr + stack_size);
-  printf("\tstack size\t= %.2f MB\n", stack_size / 1024.0 / 1024.0);
-  printf("\tguard size\t= %lu B\n", guard_size);
+  // print the stack information
+  printf("Thread %s (id=%lu) stack:\n", idx_str, thread);
+  printf("\t start address\t= %p\n", stack_addr);
+  printf("\t end address  \t= %p\n", stack_addr + stack_size);
+  printf("\t stack size   \t= %.2f MB\n", stack_size / 1024.0 / 1024.0);
+  printf("\t guard size   \t= %lu B\n", guard_size);
 }
 
 void *entry_point(void *arg) {
-  pthread_t thread = pthread_self();
-
-  int rc;
+  int ret = 0;
   pthread_attr_t attr;
-  rc = pthread_getattr_np(thread, &attr);
-  assert(rc == 0);
+  pthread_t thread = pthread_self();
+  char *idx_str = (char *) arg;
 
-  pthread_mutex_lock(&lock);
-  show_stack(&attr, thread, (char *)arg);
-  pthread_mutex_unlock(&lock);
+  // try to get the attribute of the current thread.
+  if ( (ret=pthread_getattr_np(thread, &attr)) != 0 )
+  {
+    print_error_msg(idx_str, "pthread_getattr_np failed", ret);
+    return NULL;
+  }
+
+  // pthread_mutex_lock(&lock);
+  show_stack(&attr, thread, idx_str);
+  // pthread_mutex_unlock(&lock);
 
   return NULL;
 }
 
 int main(int argc, char *argv[]) {
+  int ret = 0;
   pthread_t p1, p2;
-  int rc;
 
-  rc = pthread_create(&p1, NULL, entry_point, "1");
-  assert(rc == 0);
-  rc = pthread_create(&p2, NULL, entry_point, "2");
-  assert(rc == 0);
-
+  // run the routine in main thread
   entry_point("main");
 
-  rc = pthread_join(p1, NULL);
-  assert(rc == 0);
-  rc = pthread_join(p2, NULL);
-  assert(rc == 0);
+  // run the routine in thread p1
+  if ( (ret=pthread_create(&p1, NULL, entry_point, "p1")) != 0 )
+  {
+    print_error_msg("p1", "pthread_create failed", ret);
+    return ret;
+  }
+  if ( (ret=pthread_join(p1, NULL)) != 0 )
+  {
+    print_error_msg("p1", "pthread_join failed", ret);
+  }
+  
+  // run the routine in thread p2
+  if ( (ret=pthread_create(&p2, NULL, entry_point, "p2")) != 0 )
+  {
+    print_error_msg("p2", "pthread_create failed", ret);
+    return ret;
+  }
+  if ( (ret=pthread_join(p2, NULL)) != 0 )
+  {
+    print_error_msg("p2", "pthread_join failed", ret);
+  }
 
-  return 0;
+  return ret;
 }
