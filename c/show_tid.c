0263206703f7a65576a0685373f1063c65ab367d
iamhyc <sudofree@163.com>
2021-09-10 16:39:17 +0800

    update show_tid

--- c/show_tid.c
+++ c/show_tid.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <sys/syscall.h>
@@ -7,40 +6,42 @@
 #define gettidv1() syscall(__NR_gettid) // new form
 #define gettidv2() syscall(SYS_gettid)  // traditional form
 
-void *ThreadFunc1(void *arg) {
-  printf("the pthread_1 id is %ld\n", pthread_self());
-  printf("the thread_1's PID is %d\n", getpid());
-  printf("The LWP (TID) of thread_1 is: %ld\n", (long int)gettidv1());
+void *entry_point(void *arg)
+{
+  char *idx_str = (char *) arg;
 
-  return 0;
-}
+  printf("the pthread_%s id is %ld\n", idx_str, pthread_self());
+  printf("the thread_%s's PID is %d\n", idx_str, getpid());
+  printf("The LWP (TID) of thread_%s is: %d\n", idx_str, (unsigned int)gettidv1());
+  printf("\n");
 
-void *ThreadFunc2(void *arg) {
-  printf("the pthread_2 id is %ld\n", pthread_self());
-  printf("the thread_2's PID is %d\n", getpid());
-  printf("The LWP (TID) of thread_2 is: %ld\n", (long int)gettidv1());
-
-  return 0;
+  return NULL;
 }
 
 int main(int argc, char *argv[]) {
+  int ret = 0;
   pthread_t p1, p2;
-  int rc;
 
-  printf("the main thread's pthread id is %ld\n", pthread_self());
-  printf("the main thread's PID is %d\n", getpid());
-  printf("The LWP of main thread is: %ld\n", (long int)gettidv1());
+  printf("The main thread's pthread id is %ld\n", pthread_self());
+  printf("The main thread's PID is %d\n", getpid());
+  printf("The LWP of main thread is: %d\n", (unsigned int)gettidv1());
+  printf("\n");
 
   // create two threads
-  rc = pthread_create(&p1, NULL, ThreadFunc2, NULL);
-  assert(rc == 0);
-  rc = pthread_create(&p2, NULL, ThreadFunc1, NULL);
-  assert(rc == 0);
+  if ( (ret=pthread_create(&p1, NULL, entry_point, "1")) != 0 )
+  {
+    fprintf(stderr, "pthread_create failed for thread_1\n");
+    return ret;
+  }
+  if ( (ret=pthread_create(&p2, NULL, entry_point, "2")) != 0 )
+  {
+    fprintf(stderr, "pthread_create failed for thread_2\n");
+    return ret;
+  }
 
-  rc = pthread_join(p1, NULL);
-  assert(rc == 0);
-  rc = pthread_join(p2, NULL);
-  assert(rc == 0);
+  // join to terminate
+  pthread_join(p1, NULL);
+  pthread_join(p2, NULL);
 
-  return 0;
+  return ret;
 }
