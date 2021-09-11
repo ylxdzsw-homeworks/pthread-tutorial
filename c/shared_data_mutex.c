a3cc5eb327851067305898a7c830556319ae73ab
iamhyc <sudofree@163.com>
2021-09-11 11:08:50 +0800

    update shared_Data_mutex.c

--- c/shared_data_mutex.c
+++ c/shared_data_mutex.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <assert.h>
 
 static volatile int counter = 0;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
@@ -29,18 +28,21 @@ void *entry_point(void *arg) {
 
 int main(int argc, char *argv[]) {
     pthread_t p1, p2;
+
     printf("main: begin with counter = %d\n", counter);
-    int rc;
-    rc = pthread_create(&p1, NULL, entry_point, (void *)"A");
-    assert(rc == 0);
-    rc = pthread_create(&p2, NULL, entry_point, (void *)"B");
-    assert(rc == 0);
+
+    if ( pthread_create(&p1, NULL, entry_point, "A") != 0 )
+    {
+        fprintf(stderr, "pthread_create failed for thread A.\n");
+    }
+    if ( pthread_create(&p2, NULL, entry_point, "B") != 0 )
+    {
+        fprintf(stderr, "pthread_create failed for thread B.\n");
+    }
 
     // join waits for the threads to finish
-    rc = pthread_join(p1, NULL);
-    assert(rc == 0);
-    rc = pthread_join(p2, NULL);
-    assert(rc == 0);
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
     printf("main: done with counter = %d\n", counter);
     return 0;
 }
