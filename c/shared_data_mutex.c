4249a3c97d83f43d9c4da395b10dfa80c33e3a1c
iamhyc <sudofree@163.com>
2021-09-11 11:12:08 +0800

    restore shared_Data_mutex.c

--- c/shared_data_mutex.c
+++ c/shared_data_mutex.c
@@ -28,21 +28,18 @@ void *entry_point(void *arg) {
 
 int main(int argc, char *argv[]) {
     pthread_t p1, p2;
-
     printf("main: begin with counter = %d\n", counter);
-
-    if ( pthread_create(&p1, NULL, entry_point, "A") != 0 )
-    {
-        fprintf(stderr, "pthread_create failed for thread A.\n");
-    }
-    if ( pthread_create(&p2, NULL, entry_point, "B") != 0 )
-    {
-        fprintf(stderr, "pthread_create failed for thread B.\n");
-    }
+    int rc;
+    rc = pthread_create(&p1, NULL, entry_point, (void *)"A");
+    assert(rc == 0);
+    rc = pthread_create(&p2, NULL, entry_point, (void *)"B");
+    assert(rc == 0);
 
     // join waits for the threads to finish
-    pthread_join(p1, NULL);
-    pthread_join(p2, NULL);
+    rc = pthread_join(p1, NULL);
+    assert(rc == 0);
+    rc = pthread_join(p2, NULL);
+    assert(rc == 0);
     printf("main: done with counter = %d\n", counter);
     return 0;
 }
